a24d8b9f9460091d98fb2a1b52b40de438c53bec
nicoviarnes <nlviarnes@gmail.com>
2019-04-24 23:57:23 -0700

    fix port process.env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var cheerio = require("cheerio");
 var db = require("./models");
 
 // Set port to listen on
-var PORT = 3000;
+var PORT = process.env.PORT || 3000
 
 // Initialize Express
 var app = express();
